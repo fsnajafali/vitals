d77b04b299d96cfe302897a226f967e232d7a1e9
mpara0 <monicapara2019@gmail.com>
2020-09-12 13:30:14 -0500

    added phone intent

--- app/src/main/java/com/example/vitals/MainActivity.java
+++ app/src/main/java/com/example/vitals/MainActivity.java
@@ -1,13 +1,18 @@
 package com.example.vitals;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.content.Intent;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,9 +35,23 @@ public class MainActivity extends AppCompatActivity {
             }
         });
          ***/
-        //button to call 911
-        //resource to look at: https://www.tutorialspoint.com/android/android_phone_calls.htm
-        //delete this after - only did this to see if commit works
+
+        //calls designated phone number through intent but isn't working :(
+        //reference: https://www.tutorialspoint.com/android/android_phone_calls.htm
+        button = (Button) findViewById(R.id.emergency);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
+                Intent callIntent = new Intent(Intent.ACTION_CALL);
+                callIntent.setData(Uri.parse("tel:76743532"));
+
+                if (ActivityCompat.checkSelfPermission(MainActivity.this,
+                        Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                    return;
+                }
+                startActivity(callIntent);
+            }
+        });
 
 
 
