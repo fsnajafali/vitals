3ccdeca7fea85d055a528508c98a87f5cab06b66
mpara0 <monicapara2019@gmail.com>
2020-09-12 15:40:22 -0500

    added phone intent

--- app/src/main/java/com/example/vitals/MainActivity.java
+++ app/src/main/java/com/example/vitals/MainActivity.java
@@ -1,12 +1,19 @@
 package com.example.vitals;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import android.os.Bundle;
 import android.view.View;
 import android.content.Intent;
+import android.widget.Button;
+import android.net.Uri;
+import android.Manifest;
+import android.content.pm.PackageManager;
+
 
 public class MainActivity extends AppCompatActivity {
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +28,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         //button to navigate to location page
-        /****
         findViewById(R.id.locate).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -29,10 +35,23 @@ public class MainActivity extends AppCompatActivity {
                 MainActivity.this.startActivity(intent);
             }
         });
-         ***/
+
         //button to call 911
         //resource to look at: https://www.tutorialspoint.com/android/android_phone_calls.htm
-        //delete this after - only did this to see if commit works
+        button = (Button) findViewById(R.id.emergency);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
+                Intent callIntent = new Intent(Intent.ACTION_CALL);
+                callIntent.setData(Uri.parse("tel:0377778888"));
+
+                if (ActivityCompat.checkSelfPermission(MainActivity.this,
+                        Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                    return;
+                }
+                startActivity(callIntent);
+            }
+        });
 
 
 
